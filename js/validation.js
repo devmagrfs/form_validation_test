e9740d3f945880522b0f102d3fc544513b7094b5
devmagrfs <devmagrfs@gmail.com>
2021-11-23 10:55:13 +0900

    step3 email 인증 함수

--- js/validation.js
+++ js/validation.js
@@ -3,25 +3,43 @@ const btnSubmit = document. querySelector("input[type=submit]");
 
 
 btnSubmit.addEventListener("click", (e) => {
-    if(!isTxt('userid')) e.preventDefault();
+    if(!isTxt('userid', 5)) e.preventDefault();
+    if(!isTxt('comments', 20)) e.preventDefault();
+    if(!isEmail('email')) e.preventDefault();
 });
 
-function isTxt(name){
-    var input = form.querySelector("input[name=${name}]");
+function isTxt(name, len){
+    var input = form.querySelector(`[name=${name}]`);
     var txt = input.value;
 
-    if(txt !== ''){
+    if(txt.length > len){
         let errMsgs = input.closest("td").querySelector("p");
         if(errMsg.length > 0)  input.closest("td").querySelector("p").remove();
 
         return true;
     } else {
-
         let errMsgs = input.closest("td").querySelector("p");
         if(errMsg.length > 0)  input.closest("td").querySelector("p").remove();
 
         var errMsg = document.createElement("p");
-        errMsg.append("텍스트를 입력하세요.");
+        errMsg.append(`텍스트를 ${len}글자 이상 입력하세요.`);
+        input.closest("td").append(errMsg);
+        return false;
+    }
+}
+
+function isEmail(name){
+    var input = form.querySelector(`[name=${name}]`);
+    var txt = input.value;
+
+    if(txt.length > 0){
+        return true;
+    } else {
+        var errMsgs = input.closest("td").querySelector("p");
+        if(errMsg.length > 0) input.closest("td").querySelector("p").remove();
+
+        var errMsg = document.createElement("p");
+        errMsg.append("이메일 주소를 입력해주세요.");
         input.closest("td").append(errMsg);
         return false;
     }
